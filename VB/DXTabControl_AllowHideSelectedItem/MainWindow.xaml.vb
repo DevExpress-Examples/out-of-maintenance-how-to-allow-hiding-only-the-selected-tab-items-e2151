d979cb7061cd529a795c24577064498b8b400812
DevExpressExampleBot <fakeEmail>
2022-12-16 18:24:43 +0400

    Source auto update [skip ci]

--- VB/DXTabControl_AllowHideSelectedItem/MainWindow.xaml.vb
+++ VB/DXTabControl_AllowHideSelectedItem/MainWindow.xaml.vb
@@ -1,27 +1,26 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Utils
 Imports DevExpress.Xpf.Core
 Imports DXExample.DemoData
 
 Namespace DXTabControl_AllowHideSelectedItem
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub LayoutRoot_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			tabControl.ItemsSource = Invoice.GetData()
-		End Sub
-		Private Sub tabControl_SelectionChanged(ByVal sender As Object, ByVal e As TabControlSelectionChangedEventArgs)
-			Dim oldItem As DXTabItem = tabControl.GetTabItem(e.OldSelectedIndex)
-			Dim newItem As DXTabItem = tabControl.GetTabItem(tabControl.SelectedIndex)
-			If oldItem IsNot Nothing Then
-				oldItem.AllowHide = DefaultBoolean.Default
-			End If
-			If newItem IsNot Nothing Then
-				newItem.AllowHide = DefaultBoolean.True
-			End If
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub LayoutRoot_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.tabControl.ItemsSource = Invoice.GetData()
+        End Sub
+
+        Private Sub tabControl_SelectionChanged(ByVal sender As Object, ByVal e As TabControlSelectionChangedEventArgs)
+            Dim oldItem As DXTabItem = Me.tabControl.GetTabItem(e.OldSelectedIndex)
+            Dim newItem As DXTabItem = Me.tabControl.GetTabItem(Me.tabControl.SelectedIndex)
+            If oldItem IsNot Nothing Then oldItem.AllowHide = DefaultBoolean.Default
+            If newItem IsNot Nothing Then newItem.AllowHide = DefaultBoolean.True
+        End Sub
+    End Class
 End Namespace
